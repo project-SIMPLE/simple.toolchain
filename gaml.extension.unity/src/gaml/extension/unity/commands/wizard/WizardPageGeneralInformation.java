8fb01c985988198d3bfcd8d53070bd2ee3eb4f71
AlexisDrogoul <alexis.drogoul@gmail.com>
2024-02-22 08:28:48 +0700

    Changes for 2024-06

--- gaml.extension.unity/src/gaml/extension/unity/commands/wizard/WizardPageGeneralInformation.java
+++ gaml.extension.unity/src/gaml/extension/unity/commands/wizard/WizardPageGeneralInformation.java
@@ -1,3 +1,13 @@
+/*******************************************************************************************************
+ *
+ * WizardPageGeneralInformation.java, in gaml.extension.unity, is part of the source code of the GAMA modeling and
+ * simulation platform (v.1.9.3).
+ *
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ *
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ *
+ ********************************************************************************************************/
 package gaml.extension.unity.commands.wizard;
 
 import java.util.ArrayList;
@@ -5,8 +15,6 @@ import java.util.List;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
@@ -17,113 +25,119 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-import msi.gama.kernel.experiment.IExperimentPlan;
-import msi.gama.kernel.model.IModel;
+import gama.core.kernel.experiment.IExperimentPlan;
+import gama.core.kernel.model.IModel;
 
+/**
+ * The Class WizardPageGeneralInformation.
+ */
 public class WizardPageGeneralInformation extends WizardPage {
 
+	/** The model. */
 	IModel model;
 
-    private Composite container;
-    
-    VRModelGenerator generator;
-    
-    WizardPageDisplay wDisplay;
-    
-	protected WizardPageGeneralInformation(String path, IModel model, VRModelGenerator gen) {
-		 super("GeneralInformation");
-		 setTitle("Define the general information to define the VR experiment");
-		 setDescription("Please enter information about VR experiment");
-		 this.model = model;
-		 this.generator = gen;
-		 gen.setModelName(model.getName() + "_VR");
-		 gen.setModelPath(path);
+	/** The container. */
+	private Composite container;
+
+	/** The generator. */
+	VRModelGenerator generator;
+
+	/** The w display. */
+	WizardPageDisplay wDisplay;
+
+	/**
+	 * Instantiates a new wizard page general information.
+	 *
+	 * @param path
+	 *            the path
+	 * @param model
+	 *            the model
+	 * @param gen
+	 *            the gen
+	 */
+	protected WizardPageGeneralInformation(final String path, final IModel model, final VRModelGenerator gen) {
+		super("GeneralInformation");
+		setTitle("Define the general information to define the VR experiment");
+		setDescription("Please enter information about VR experiment");
+		this.model = model;
+		this.generator = gen;
+		gen.setModelName(model.getName() + "_VR");
+		gen.setModelPath(path);
 	}
-	
 
 	@Override
-	public void createControl(Composite parent) {
+	public void createControl(final Composite parent) {
 		container = new Composite(parent, SWT.NONE);
-		 container.setLayout(new FillLayout(SWT.VERTICAL));
-		/* Group groupConnection = new Group(container, SWT.NONE);
-		 groupConnection.setLayout(new GridLayout(2, false));
-		 groupConnection.setText("Information about the connection");
-			
-		 Label lp = new Label(groupConnection, SWT.LEFT);
-			lp.setText("Port:" );
-			Text tp =  new Text(groupConnection, SWT.BORDER);
-			tp.setText(generator.getPort().toString());
-		    tp.addModifyListener(new ModifyListener() {
-				
-				@Override
-			
-				public void modifyText(ModifyEvent e) {
-					Integer port = Integer.decode(tp.getText());
-					if (port != null)
-						generator.setPort(port);
-				}
-		    });*/
-			
-			 Group groupExperiment = new Group(container, SWT.NONE);
-			 groupExperiment.setLayout(new GridLayout(2, false));
-			 groupExperiment.setText("Information about the experiment");
-				
+		container.setLayout(new FillLayout(SWT.VERTICAL));
+		/*
+		 * Group groupConnection = new Group(container, SWT.NONE); groupConnection.setLayout(new GridLayout(2, false));
+		 * groupConnection.setText("Information about the connection");
+		 * 
+		 * Label lp = new Label(groupConnection, SWT.LEFT); lp.setText("Port:" ); Text tp = new Text(groupConnection,
+		 * SWT.BORDER); tp.setText(generator.getPort().toString()); tp.addModifyListener(new ModifyListener() {
+		 * 
+		 * @Override
+		 * 
+		 * public void modifyText(ModifyEvent e) { Integer port = Integer.decode(tp.getText()); if (port != null)
+		 * generator.setPort(port); } });
+		 */
 
-				Label lc = new Label(groupExperiment, SWT.LEFT);
-				lc.setText("Minimium duration of a cycle in s (minimum_cycle_duration):" );
-				Text tc =  new Text(groupExperiment, SWT.BORDER);
-				tc.setText(generator.getMinimumCycleDuration().toString());
-				tc.addModifyListener(new ModifyListener() {
-					
-					@Override
-				
-					public void modifyText(ModifyEvent e) {
-						Double duration = Double.valueOf(tc.getText());
-						if (duration != null)
-							generator.setMinimumCycleDuration(duration);
-					}
-			    });
-				Label lxp = new Label(groupExperiment, SWT.LEFT);
-				lxp.setText("Main Experiment:" );
-			
-				Combo cXp = new Combo(groupExperiment, SWT.READ_ONLY);
-				 List<String> items = new ArrayList<String>();
-				 for (IExperimentPlan ep : model.getExperiments()) {
-					 items.add(ep.getName());
-				 }
-				 
-				 cXp.setItems((String[]) items.toArray(new String[items.size()]));
-				 if (! items.isEmpty()) {
-					 cXp.setText(items.get(0));
-					  generator.setExperimentName(cXp.getText());
-					  wDisplay.updateExperiment();
-				    	
-				 }
+		Group groupExperiment = new Group(container, SWT.NONE);
+		groupExperiment.setLayout(new GridLayout(2, false));
+		groupExperiment.setText("Information about the experiment");
 
-				 cXp.addSelectionListener(new SelectionAdapter() {
-				      public void widgetDefaultSelected(SelectionEvent e) {
-				    	  generator.setExperimentName(cXp.getText());
-				    	  wDisplay.updateExperiment();
-				      }
-				    });
+		Label lc = new Label(groupExperiment, SWT.LEFT);
+		lc.setText("Minimium duration of a cycle in s (minimum_cycle_duration):");
+		Text tc = new Text(groupExperiment, SWT.BORDER);
+		tc.setText(generator.getMinimumCycleDuration().toString());
+		tc.addModifyListener(e -> {
+			Double duration = Double.valueOf(tc.getText());
+			if (duration != null) { generator.setMinimumCycleDuration(duration); }
+		});
+		Label lxp = new Label(groupExperiment, SWT.LEFT);
+		lxp.setText("Main Experiment:");
 
-		
-					 
-		 setControl(container);
-	       
-		
+		Combo cXp = new Combo(groupExperiment, SWT.READ_ONLY);
+		List<String> items = new ArrayList<>();
+		for (IExperimentPlan ep : model.getExperiments()) { items.add(ep.getName()); }
+
+		cXp.setItems(items.toArray(new String[items.size()]));
+		if (!items.isEmpty()) {
+			cXp.setText(items.get(0));
+			generator.setExperimentName(cXp.getText());
+			wDisplay.updateExperiment();
+
+		}
+
+		cXp.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(final SelectionEvent e) {
+				generator.setExperimentName(cXp.getText());
+				wDisplay.updateExperiment();
+			}
+		});
+
+		setControl(container);
+
 	}
 
-
+	/**
+	 * Gets the w display.
+	 *
+	 * @return the w display
+	 */
 	public WizardPageDisplay getwDisplay() {
 		return wDisplay;
 	}
 
-
-	public void setwDisplay(WizardPageDisplay wDisplay) {
+	/**
+	 * Sets the w display.
+	 *
+	 * @param wDisplay
+	 *            the new w display
+	 */
+	public void setwDisplay(final WizardPageDisplay wDisplay) {
 		this.wDisplay = wDisplay;
 	}
-	
-	
 
 }
--- gaml.extension.unity/src/gaml/extension/unity/commands/wizard/WizardPagePlayer.java
+++ gaml.extension.unity/src/gaml/extension/unity/commands/wizard/WizardPagePlayer.java
@@ -1,10 +1,19 @@
+/*******************************************************************************************************
+ *
+ * WizardPagePlayer.java, in gaml.extension.unity, is part of the source code of the GAMA modeling and simulation
+ * platform (v.1.9.3).
+ *
+ * (c) 2007-2024 UMI 209 UMMISCO IRD/SU & Partners (IRIT, MIAT, TLU, CTU)
+ *
+ * Visit https://github.com/gama-platform/gama for license information and contacts.
+ *
+ ********************************************************************************************************/
 package gaml.extension.unity.commands.wizard;
 
 import java.util.List;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -16,222 +25,179 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-import msi.gama.kernel.model.IModel;
-import msi.gama.metamodel.shape.GamaPoint;
+import gama.core.kernel.model.IModel;
+import gama.core.metamodel.shape.GamaPoint;
 
+/**
+ * The Class WizardPagePlayer.
+ */
 public class WizardPagePlayer extends WizardPage {
 
+	/** The model. */
 	IModel model;
 
-    private Composite container;
-    
-    VRModelGenerator generator;
-    
-    List<String> itemsD;
-    
-    
-    
-	protected WizardPagePlayer(IModel model, VRModelGenerator gen) {
-		 super("Player");
-		 setTitle("Define the information about the player");
-		 setDescription("Please enter information about the player");
-		 setPageComplete(true);
-		 this.model = model;
-		 this.generator = gen;
-		 
+	/** The container. */
+	private Composite container;
+
+	/** The generator. */
+	VRModelGenerator generator;
+
+	/** The items D. */
+	List<String> itemsD;
+
+	/**
+	 * Instantiates a new wizard page player.
+	 *
+	 * @param model
+	 *            the model
+	 * @param gen
+	 *            the gen
+	 */
+	protected WizardPagePlayer(final IModel model, final VRModelGenerator gen) {
+		super("Player");
+		setTitle("Define the information about the player");
+		setDescription("Please enter information about the player");
+		setPageComplete(true);
+		this.model = model;
+		this.generator = gen;
+
 	}
-	
+
 	@Override
-	public void createControl(Composite parent) {
+	public void createControl(final Composite parent) {
 		container = new Composite(parent, SWT.NONE);
-		 container.setLayout(new FillLayout(SWT.VERTICAL));
-		 Group groupPlayer = new Group(container, SWT.NONE);
-		 groupPlayer.setLayout(new GridLayout(2, false));
-		 groupPlayer.setText("Information about the player");
-		
-		 Label lma = new Label(groupPlayer, SWT.LEFT);
-		 lma.setText("Min number of players" );
-			Text tma =  new Text(groupPlayer, SWT.BORDER);
-			tma.setText(generator.getMin_num_player() + "");
-			tma.addModifyListener(new ModifyListener() {
-				
-				@Override
-				public void modifyText(ModifyEvent e) {
-					Integer tami = Integer.valueOf(tma.getText());
-					if (tami != null)
-						generator.setMin_num_player(tami);
+		container.setLayout(new FillLayout(SWT.VERTICAL));
+		Group groupPlayer = new Group(container, SWT.NONE);
+		groupPlayer.setLayout(new GridLayout(2, false));
+		groupPlayer.setText("Information about the player");
+
+		Label lma = new Label(groupPlayer, SWT.LEFT);
+		lma.setText("Min number of players");
+		Text tma = new Text(groupPlayer, SWT.BORDER);
+		tma.setText(generator.getMin_num_player() + "");
+		tma.addModifyListener(e -> {
+			Integer tami = Integer.valueOf(tma.getText());
+			if (tami != null) { generator.setMin_num_player(tami); }
+		});
+
+		Label lhmaa = new Label(groupPlayer, SWT.LEFT);
+		lhmaa.setText("Has a max number of players?");
+		Button bhmaa = new Button(groupPlayer, SWT.CHECK);
+		bhmaa.setSelection(true);
+
+		Label lmaa = new Label(groupPlayer, SWT.LEFT);
+		lmaa.setText("Max number of players");
+		Text tmaa = new Text(groupPlayer, SWT.BORDER);
+		tmaa.setText(generator.getMax_num_player() + "");
+		tmaa.addModifyListener(e -> {
+			Integer tmaai = Integer.valueOf(tmaa.getText());
+			if (tmaai != null) { generator.setMax_num_player(tmaai); }
+		});
+
+		bhmaa.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(final SelectionEvent event) {
+				Button btn = (Button) event.getSource();
+				generator.setHas_max_num_player(btn.getSelection());
+				if (btn.getSelection()) {
+					tmaa.setEnabled(true);
+
+				} else {
+					tmaa.setEnabled(false);
 				}
-		    });
-			
-			Label lhmaa = new Label(groupPlayer, SWT.LEFT);
-			lhmaa.setText("Has a max number of players?" );
-			Button bhmaa = new Button(groupPlayer, SWT.CHECK);
-			bhmaa.setSelection(true);
-			 
-			Label lmaa = new Label(groupPlayer, SWT.LEFT);
-			lmaa.setText("Max number of players" );
-				Text tmaa =  new Text(groupPlayer, SWT.BORDER);
-				tmaa.setText(generator.getMax_num_player() + "");
-				tmaa.addModifyListener(new ModifyListener() {
-					
-					@Override
-					public void modifyText(ModifyEvent e) {
-						Integer tmaai = Integer.valueOf(tmaa.getText());
-						if (tmaai != null)
-							generator.setMax_num_player(tmaai);
-					}
-			    });
-				
-				bhmaa.addSelectionListener(new SelectionAdapter() {
 
-					public void widgetSelected(SelectionEvent event) {
-						Button btn = (Button) event.getSource();
-				        generator.setHas_max_num_player(btn.getSelection());
-				        if (btn.getSelection()) {
-				        	tmaa.setEnabled(true);
-				        	
-				        } else {
-				        	tmaa.setEnabled(false);
-				        }
-				           
-				    }
-				});
-			 
-			 
-		/* Label lp = new Label(groupPlayer, SWT.LEFT);
-			lp.setText("Add a player agent?" );
-			Button bt = new Button(groupPlayer, SWT.CHECK);
-			 bt.setSelection(true);
-		*/	
-			
-			Label lic = new Label(groupPlayer, SWT.LEFT);
-			lic.setText("Init location of the players" );
-			 generator.setLocationInit(new GamaPoint(50.0,50.0,0.0));
-		
-			 Group groupPlayerLoc = new Group(groupPlayer, SWT.NONE);
-			 groupPlayerLoc.setLayout(new GridLayout(6, false));
-			
-			 	Label lilx = new Label(groupPlayerLoc, SWT.LEFT);
-			 	lilx.setText("X:" );
-			 	
-			 	Text tilx =  new Text(groupPlayerLoc, SWT.BORDER);
-			 	Label lily = new Label(groupPlayerLoc, SWT.LEFT);
-			 	lily.setText("Y:" );
-			 	
-			 	Text tily =  new Text(groupPlayerLoc, SWT.BORDER);
-			 //	Label lilz = new Label(groupPlayerLoc, SWT.LEFT);
-			 //	lilz.setText("Z:" );
-			 	
-			 //	Text tilz =  new Text(groupPlayerLoc, SWT.BORDER);
-				
-			 	tilx.setText("" + generator.getLocationInit().x);
-			 	tily.setText("" + generator.getLocationInit().y);
-			 //	tilz.setText("" + generator.getLocationInit().z);
-			 	
-			 	ModifyListener ml = new ModifyListener() {
-					
-					@Override
-					public void modifyText(ModifyEvent e) {
-						Double x = Double.valueOf(tilx.getText());
-						Double y = Double.valueOf(tily.getText());
-						//Double z = Double.valueOf(tilz.getText());
-						if (x != null && y != null  )
-							generator.setLocationInit(new GamaPoint(x,y));
-							
-					}
-				};
-					
-			    
-			 	tilx.addModifyListener(ml);
-			 	tily.addModifyListener(ml);
-			 	//tilz.addModifyListener(ml);
-				
-			 	
-				 Label lpr = new Label(groupPlayer, SWT.LEFT);
-				 lpr.setText("Perception radius of the player agent:" );
-					Text tpr =  new Text(groupPlayer, SWT.BORDER);
-					tpr.setText(generator.getPlayerAgentsPerceptionRadius().toString());
-				    tpr.addModifyListener(new ModifyListener() {
-						
-						@Override
-					
-						public void modifyText(ModifyEvent e) {
-							Double papr = Double.valueOf(tpr.getText());
-							if (papr != null)
-								generator.setPlayerAgentsPerceptionRadius(papr);
-						}
-				    });
-					
-				    Label lpmd = new Label(groupPlayer, SWT.LEFT);
-				    lpmd.setText("Min distance between agents to send to be considered:" );
-						Text tmd =  new Text(groupPlayer, SWT.BORDER);
-						tmd.setText(generator.getPlayerAgentsPerceptionRadius().toString());
-						tmd.addModifyListener(new ModifyListener() {
-							
-							@Override
-						
-							public void modifyText(ModifyEvent e) {
-								Double pamd = Double.valueOf(tmd.getText());
-								if (pamd != null)
-									generator.setPlayerAgentsMinDist(pamd);
-							}
-					    });
-						
-						Label lps = new Label(groupPlayer, SWT.LEFT);
-						lps.setText("Player Size (display):" );
-							Text tps =  new Text(groupPlayer, SWT.BORDER);
-							tps.setText(generator.getPlayerSize().toString());
-							tps.addModifyListener(new ModifyListener() {
-								
-								@Override
-							
-								public void modifyText(ModifyEvent e) {
-									Double ps = Double.valueOf(tps.getText());
-									if (ps != null)
-										generator.setPlayerSize(ps);
-								}
-						    });
-							
-							Label lpc = new Label(groupPlayer, SWT.LEFT);
-							lpc.setText("Player color (display):" );
-							Text tpc =  new Text(groupPlayer, SWT.BORDER);
-							tpc.setText(generator.getPlayerColor());
-							tpc.addModifyListener(new ModifyListener() {
-								
-								@Override
-							
-								public void modifyText(ModifyEvent e) {
-									generator.setPlayerColor(tpc.getText());
-								}
-						    });
-						
-							/*bt.addSelectionListener(new SelectionAdapter() {
+			}
+		});
 
-								public void widgetSelected(SelectionEvent event) {
-									Button btn = (Button) event.getSource();
-							        generator.setHasPlayer(btn.getSelection());
-							        if (btn.getSelection()) {
-							        	tpc.setEnabled(true);
-							        	tps.setEnabled(true);
-							        	tmd.setEnabled(true);
-							        	tpr.setEnabled(true);
-							        	tilx.setEnabled(true);
-							        	tily.setEnabled(true);
-							        	tilz.setEnabled(true);
-							        } else {
-							        	tpc.setEnabled(false);
-							        	tps.setEnabled(false);
-							        	tmd.setEnabled(false);
-							        	tpr.setEnabled(false);
-							        	tilx.setEnabled(false);
-							        	tily.setEnabled(false);
-							        	tilz.setEnabled(false);
-							        }
-							           
-							    }
-							});*/
-		 setControl(container);
-			
+		/*
+		 * Label lp = new Label(groupPlayer, SWT.LEFT); lp.setText("Add a player agent?" ); Button bt = new
+		 * Button(groupPlayer, SWT.CHECK); bt.setSelection(true);
+		 */
+
+		Label lic = new Label(groupPlayer, SWT.LEFT);
+		lic.setText("Init location of the players");
+		generator.setLocationInit(new GamaPoint(50.0, 50.0, 0.0));
+
+		Group groupPlayerLoc = new Group(groupPlayer, SWT.NONE);
+		groupPlayerLoc.setLayout(new GridLayout(6, false));
+
+		Label lilx = new Label(groupPlayerLoc, SWT.LEFT);
+		lilx.setText("X:");
+
+		Text tilx = new Text(groupPlayerLoc, SWT.BORDER);
+		Label lily = new Label(groupPlayerLoc, SWT.LEFT);
+		lily.setText("Y:");
+
+		Text tily = new Text(groupPlayerLoc, SWT.BORDER);
+		// Label lilz = new Label(groupPlayerLoc, SWT.LEFT);
+		// lilz.setText("Z:" );
+
+		// Text tilz = new Text(groupPlayerLoc, SWT.BORDER);
+
+		tilx.setText("" + generator.getLocationInit().x);
+		tily.setText("" + generator.getLocationInit().y);
+		// tilz.setText("" + generator.getLocationInit().z);
+
+		ModifyListener ml = e -> {
+			Double x = Double.valueOf(tilx.getText());
+			Double y = Double.valueOf(tily.getText());
+			// Double z = Double.valueOf(tilz.getText());
+			if (x != null && y != null) { generator.setLocationInit(new GamaPoint(x, y)); }
+
+		};
+
+		tilx.addModifyListener(ml);
+		tily.addModifyListener(ml);
+		// tilz.addModifyListener(ml);
+
+		Label lpr = new Label(groupPlayer, SWT.LEFT);
+		lpr.setText("Perception radius of the player agent:");
+		Text tpr = new Text(groupPlayer, SWT.BORDER);
+		tpr.setText(generator.getPlayerAgentsPerceptionRadius().toString());
+		tpr.addModifyListener(e -> {
+			Double papr = Double.valueOf(tpr.getText());
+			if (papr != null) { generator.setPlayerAgentsPerceptionRadius(papr); }
+		});
+
+		Label lpmd = new Label(groupPlayer, SWT.LEFT);
+		lpmd.setText("Min distance between agents to send to be considered:");
+		Text tmd = new Text(groupPlayer, SWT.BORDER);
+		tmd.setText(generator.getPlayerAgentsPerceptionRadius().toString());
+		tmd.addModifyListener(e -> {
+			Double pamd = Double.valueOf(tmd.getText());
+			if (pamd != null) { generator.setPlayerAgentsMinDist(pamd); }
+		});
+
+		Label lps = new Label(groupPlayer, SWT.LEFT);
+		lps.setText("Player Size (display):");
+		Text tps = new Text(groupPlayer, SWT.BORDER);
+		tps.setText(generator.getPlayerSize().toString());
+		tps.addModifyListener(e -> {
+			Double ps = Double.valueOf(tps.getText());
+			if (ps != null) { generator.setPlayerSize(ps); }
+		});
+
+		Label lpc = new Label(groupPlayer, SWT.LEFT);
+		lpc.setText("Player color (display):");
+		Text tpc = new Text(groupPlayer, SWT.BORDER);
+		tpc.setText(generator.getPlayerColor());
+		tpc.addModifyListener(e -> generator.setPlayerColor(tpc.getText()));
+
+		/*
+		 * bt.addSelectionListener(new SelectionAdapter() {
+		 * 
+		 * public void widgetSelected(SelectionEvent event) { Button btn = (Button) event.getSource();
+		 * generator.setHasPlayer(btn.getSelection()); if (btn.getSelection()) { tpc.setEnabled(true);
+		 * tps.setEnabled(true); tmd.setEnabled(true); tpr.setEnabled(true); tilx.setEnabled(true);
+		 * tily.setEnabled(true); tilz.setEnabled(true); } else { tpc.setEnabled(false); tps.setEnabled(false);
+		 * tmd.setEnabled(false); tpr.setEnabled(false); tilx.setEnabled(false); tily.setEnabled(false);
+		 * tilz.setEnabled(false); }
+		 * 
+		 * } });
+		 */
+		setControl(container);
+
 	}
 
 }
