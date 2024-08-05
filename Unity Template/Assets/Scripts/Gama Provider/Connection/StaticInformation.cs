cd26b5b07c632372b9470ac47886735c0e066d4d
ptaillandier <patrick.taillandier@gmail.com>
2024-08-05 18:44:19 +0700

    fixes an error regaring ID of the players for some configurations

--- Unity Template/Assets/Scripts/Gama Provider/Connection/StaticInformation.cs
+++ Unity Template/Assets/Scripts/Gama Provider/Connection/StaticInformation.cs
@@ -1,5 +1,5 @@
 using System.Net;
-
+using System;
 public static class StaticInformation
 {
     public static string endOfGame { get; set; }
@@ -10,10 +10,18 @@ public static class StaticInformation
         if (connectionId == null || connectionId.Length == 0)
         {
             string hostName = Dns.GetHostName(); // Retrive the Name of HOST
-            string myIP = Dns.GetHostByName(hostName).AddressList[0].MapToIPv4().ToString();
+           try
+            {
+                string myIP = Dns.GetHostByName(hostName).AddressList[0].MapToIPv4().ToString();
 
-             string lastIP = myIP.Contains(".") ? myIP.Split(".")[3] : "0";
-            connectionId =  "Player_" + lastIP;// + lastIP;
+
+                string lastIP = myIP.Contains(".") ? myIP.Split(".")[3] : "0";
+                connectionId = "Player_" + lastIP;// + lastIP;
+            } catch(Exception e)
+            {
+                connectionId = hostName;
+            }
+           
         }
         return connectionId;
     }
