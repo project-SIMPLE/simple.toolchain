a10e86c4e1a486ecb87075fbbf2caa0b8a3639e2
Arthur Brugière <16764085+RoiArthurB@users.noreply.github.com>
2026-01-14 15:14:04 +0700

    fix: Revert WS DefaultIP address to 'localhost'

--- Unity Template VR/Assets/Scripts/Gama Provider/Connection/WebSocketConnector.cs
+++ Unity Template VR/Assets/Scripts/Gama Provider/Connection/WebSocketConnector.cs
@@ -7,7 +7,7 @@ using NativeWebSocket;
 public abstract class WebSocketConnector : MonoBehaviour
 {
 
-    protected string DefaultIP = "10.217.129.233";//"localhost";
+    protected string DefaultIP = "localhost";
     protected string DefaultPort = "8080";
 
 
