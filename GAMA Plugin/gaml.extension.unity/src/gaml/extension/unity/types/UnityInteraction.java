844549d33e2f06a21a76e249ea42e44d3068cf8b
AlexisDrogoul <alexis.drogoul@gmail.com>
2026-04-06 17:45:11 +0700

    Update UnityInteraction.java

--- GAMA Plugin/gaml.extension.unity/src/gaml/extension/unity/types/UnityInteraction.java
+++ GAMA Plugin/gaml.extension.unity/src/gaml/extension/unity/types/UnityInteraction.java
@@ -14,15 +14,16 @@ import java.util.Map;
 
 import gama.annotations.doc;
 import gama.annotations.variable;
+import gama.annotations.vars;
 import gama.api.exceptions.GamaRuntimeException;
 import gama.api.gaml.types.IType;
+import gama.api.gaml.types.Types;
 import gama.api.runtime.scope.IScope;
 import gama.api.types.list.IList;
 import gama.api.types.map.GamaMapFactory;
 import gama.api.types.misc.IValue;
 import gama.api.utils.json.IJson;
 import gama.api.utils.json.IJsonValue;
-import gama.annotations.vars;
 
 /**
  * The Class BDIPlan.
@@ -34,59 +35,91 @@ import gama.annotations.vars;
 		name = "has_collider",
 		type = IType.BOOL,
 		doc = @doc ("has the geometry a collider")),
-@variable (
-		name = "constraints",
-		type = IType.LIST,
-		of = IType.BOOL,
-		doc = @doc ("Constraints for the movement of the geometry - [Freeze x position, Freeze y position, Freeze Z position, Freeze x rotation, Freeze y rotation, Freeze Z rotation]")),
-@variable (
-		name = "is_interactable",
-		type = IType.BOOL,
-		doc = @doc ("is the geometry interactable")),
-@variable (
-	name = "is_grabable",
-	type = IType.BOOL,
-	doc = @doc ("is the geometry grabable (interaction with Ray interactor otherwise"))
+		@variable (
+				name = "constraints",
+				type = IType.LIST,
+				of = IType.BOOL,
+				doc = @doc ("Constraints for the movement of the geometry - [Freeze x position, Freeze y position, Freeze Z position, Freeze x rotation, Freeze y rotation, Freeze Z rotation]")),
+		@variable (
+				name = "is_interactable",
+				type = IType.BOOL,
+				doc = @doc ("is the geometry interactable")),
+		@variable (
+				name = "is_grabable",
+				type = IType.BOOL,
+				doc = @doc ("is the geometry grabable (interaction with Ray interactor otherwise"))
 
 })
 public class UnityInteraction implements IValue {
 
-	private boolean collider;
-	private boolean interactable;
-	private boolean grabable;
-	private IList<Boolean> constraints;
-	
-	public UnityInteraction( boolean collider,
-			boolean interactable, boolean grabable, IList<Boolean> constraints) {
-		super();
+	/** The collider. */
+	private final boolean collider;
+
+	/** The interactable. */
+	private final boolean interactable;
+
+	/** The grabable. */
+	private final boolean grabable;
+
+	/** The constraints. */
+	private final IList<Boolean> constraints;
+
+	/**
+	 * Instantiates a new unity interaction.
+	 *
+	 * @param collider
+	 *            the collider
+	 * @param interactable
+	 *            the interactable
+	 * @param grabable
+	 *            the grabable
+	 * @param constraints
+	 *            the constraints
+	 */
+	public UnityInteraction(final boolean collider, final boolean interactable, final boolean grabable, final IList<Boolean> constraints) {
 		this.collider = collider;
 		this.interactable = interactable;
 		this.grabable = grabable;
 		this.constraints = constraints;
 	}
-	
 
-	
-	public boolean isCollider() {
-		return collider;
-	}
+	/**
+	 * Checks if is collider.
+	 *
+	 * @return true, if is collider
+	 */
+	public boolean isCollider() { return collider; }
 
-	public boolean isInteractable() {
-		return interactable;
-	}
+	/**
+	 * Checks if is interactable.
+	 *
+	 * @return true, if is interactable
+	 */
+	public boolean isInteractable() { return interactable; }
 
-	public boolean isGrabable() {
-		return grabable;
-	}
-	public IList<Boolean> getConstraints() {
-		return constraints;
-	}
-	
+	/**
+	 * Checks if is grabable.
+	 *
+	 * @return true, if is grabable
+	 */
+	public boolean isGrabable() { return grabable; }
 
+	/**
+	 * Gets the constraints.
+	 *
+	 * @return the constraints
+	 */
+	public IList<Boolean> getConstraints() { return constraints; }
+
+	/**
+	 * To map.
+	 *
+	 * @return the map
+	 */
 	public Map<String, Object> toMap() {
 		Map<String, Object> map = GamaMapFactory.create();
 		map.put("constraints", constraints);
-		map.put("isInteractable",interactable);
+		map.put("isInteractable", interactable);
 		map.put("isGrabable", grabable);
 		map.put("hasCollider", collider);
 		return map;
@@ -94,37 +127,27 @@ public class UnityInteraction implements IValue {
 
 	@Override
 	public String toString() {
-		return (collider ? "- has_collider" : "") +  (interactable ? "- is_interactable" : "") + (grabable ? "- is_grabable" : "") + " - "+constraints; 
+		return (collider ? "- has_collider" : "") + (interactable ? "- is_interactable" : "")
+				+ (grabable ? "- is_grabable" : "") + " - " + constraints;
 	}
 
-	
 	@Override
-	public String stringValue(IScope scope) throws GamaRuntimeException {
+	public String stringValue(final IScope scope) throws GamaRuntimeException {
 		return toString();
 	}
 
-
 	@Override
-	public IValue copy(IScope scope) throws GamaRuntimeException {
+	public IValue copy(final IScope scope) throws GamaRuntimeException {
 		return null;
 	}
 
-
+	@Override
+	public IType<?> getGamlType() { return Types.get(UnityInteractionType.UNITYINTERACTIONTYPE_ID); }
 
 	@Override
-	public IType<?> getGamlType() {
+	public IJsonValue serializeToJson(final IJson json) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
-
-	@Override
-	public IJsonValue serializeToJson(IJson json) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
 
 }
